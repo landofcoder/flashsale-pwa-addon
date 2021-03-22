04058e12e1301a1a4a1cd02ccd3c742693257ebc
landofcoder <landofcoder@gmail.com>
2021-03-22 10:48:47 +0700

    init module files

---
+++ src/componentOverrideMapping.js
@@ -0,0 +1,5 @@
+module.exports = componentOverride = {
+	[`@magento/venia-ui/lib/components/ProductFullDetail/productFullDetail.js`]: '@landofcoder/flashsales-module/src/override/productFullDetail.js',
+	[`@magento/venia-ui/lib/components/Gallery/item.js`]: '@landofcoder/flashsales-module/src/override/item.js',
+	[`@magento/venia-ui/lib/components/CategoryList/categoryList.js`]: '@landofcoder/flashsales-module/src/override/categoryList.js'
+};
